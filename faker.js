e70958304618804734f41b5829ab979c6ac3a18a
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-09-01 10:12:25 -0600

    faker is cool

---
+++ faker.js
@@ -0,0 +1,8 @@
+var faker = require('faker');
+
+var randomName = faker.name.findName(); // Rowan Nikolaus
+var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+var randomCard = faker.helpers.createCard(); // random contact card containing many properties
+var image = faker.image.image();
+
+console.log(image);
